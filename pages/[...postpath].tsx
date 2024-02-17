fda89c816ac1a13d488d47348a0f5e9b627963fb
UsaTime <160224255+Yanur888@users.noreply.github.com>
2024-02-17 02:31:10 -1000

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -18,7 +18,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					'https://usalivetv.000webhostapp.com/'  + encodeURI(path as string)
+					'https://usalivetv.000webhostapp.com/'
 				}`,
 			},
 		};
